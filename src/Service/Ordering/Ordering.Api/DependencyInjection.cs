3dee4b7440f1a07fd43ffe5b93636efa5501949f
tech4m <amitleo81@gmail.com>
2024-10-07 10:21:34 +0530

    Code changes for ordering apis

--- src/Service/Ordering/Ordering.Api/DependencyInjection.cs
+++ src/Service/Ordering/Ordering.Api/DependencyInjection.cs
@@ -1,18 +1,42 @@
-﻿namespace Ordering.Api;
+﻿using BuildingBlocks.Exceptions.Handler;
+
+namespace Ordering.Api;
 
 public static class DependencyInjection
 {
     //before building the application
     public static IServiceCollection AddApiServices(this IServiceCollection services)
     {
+        services.AddCarter(); 
+        var assembly = typeof(Program).Assembly;
         services.AddCarter();
+
+        services.AddMediatR(config =>
+        {
+            config.RegisterServicesFromAssemblies(assembly);
+            config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(assembly);
+
+
+        //services.AddHealthChecks()
+        //    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddExceptionHandler<CustomExceptionHandler>();
+
         return services;
     }
 
     //After building the applciation
     public static WebApplication UseApiServices(this WebApplication app)
-    {
+    { 
         app.MapCarter();
+        app.UseExceptionHandler(options => { });
         return app;
     }
 }
--- src/Service/Ordering/Ordering.Api/Program.cs
+++ src/Service/Ordering/Ordering.Api/Program.cs
@@ -4,6 +4,7 @@ using BuildingBlocks.Exceptions.Handler;
 using Ordering.Api;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,48 +15,27 @@ builder.Services
     .AddInfrastructureServices(builder.Configuration)
     .AddApiServices();
 
-var assembly = typeof(Program).Assembly;
-builder.Services.AddCarter();
-
-builder.Services.AddMediatR(config =>
-{
-    config.RegisterServicesFromAssemblies(assembly);
-    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-});
-
-builder.Services.AddValidatorsFromAssembly(assembly);
 
 builder.Services.AddMarten(opts =>
 {
-    opts.Connection(builder.Configuration.GetConnectionString("Database")!); 
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
-
-//builder.Services.AddHealthChecks()
-//    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
- 
 
 var app = builder.Build();
 
-app.MapCarter();
-app.UseExceptionHandler(options => { });
+app.UseApiServices();
 //app.UseHealthChecks("/health", new HealthCheckOptions
 //{
 //    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-//});
-
+//}); 
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    await app.InitialiseDatabaseAsync();
 }
 
-app.UseHttpsRedirection(); 
-app.Run(); 
+app.UseHttpsRedirection();
+app.Run();
